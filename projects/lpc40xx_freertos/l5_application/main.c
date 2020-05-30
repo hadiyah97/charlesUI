41deb4d55245e8a978503f23c9d979a5937ff72e
Scott LoCascio <scottlocascio@gmail.com>
2020-05-30 16:10:43 -0700

    updated for new motor controllers

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -6,13 +6,15 @@
 #include "gpio.h"
 #include "pwm1.h"
 
+gpio_s dir, pwm;
+
 void motor_task(void *params);
 
-gpio_s dir1 = {2, 7};
-gpio_s dir2 = {2, 6};
-gpio_s pwm = {2, 5};
-
 int main(void) {
+  dir = gpio__construct_with_function(GPIO__PORT_2, 6, 0);
+  pwm = gpio__construct_with_function(GPIO__PORT_2, 5, 1);
+
+  gpio__set_as_output(dir);
 
   xTaskCreate(motor_task, "motor task", (4096 / sizeof(void *)), NULL, 1, NULL);
 
@@ -26,19 +28,12 @@ void motor_task(void *params) {
 
   pwm1__init_single_edge(1000);
 
-  gpio__set_function(dir1, 0);
-  gpio__set_function(dir2, 0);
-  gpio__set_function(pwm, 1);
-
-  gpio__set_as_output(dir1);
-  gpio__set_as_output(dir2);
-
-  gpio__set(dir1);
-  gpio__reset(dir2);
-
   printf("Done with motor function setup, entering loop\n");
 
   while (1) {
+    // forward ramp up
+    gpio__set(dir);
+
     printf("20%% duty\n");
     pwm1__set_duty_cycle(PWM1__2_5, 20);
     vTaskDelay(1500); // 1500ms
@@ -50,5 +45,30 @@ void motor_task(void *params) {
     printf("80%% duty\n");
     pwm1__set_duty_cycle(PWM1__2_5, 80);
     vTaskDelay(1500);
+
+    // stop motor
+    printf("0%% duty (stopped)\n");
+    pwm1__set_duty_cycle(PWM1__2_5, 0);
+    vTaskDelay(500);
+
+    // reverse ramp up
+    gpio__reset(dir);
+
+    printf("20%% duty\n");
+    pwm1__set_duty_cycle(PWM1__2_5, 20);
+    vTaskDelay(1500); // 1500ms
+
+    printf("50%% duty\n");
+    pwm1__set_duty_cycle(PWM1__2_5, 50);
+    vTaskDelay(1500);
+
+    printf("80%% duty\n");
+    pwm1__set_duty_cycle(PWM1__2_5, 80);
+    vTaskDelay(1500);
+
+    // stop motor
+    printf("0%% duty (stopped)\n");
+    pwm1__set_duty_cycle(PWM1__2_5, 0);
+    vTaskDelay(500);
   }
 }
