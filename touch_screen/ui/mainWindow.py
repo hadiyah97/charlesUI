a043e0ca38ee96f30ea5e0aeeb6153ee1085a1cd
Sean L <nolinyc@gmail.com>
2020-08-02 15:16:32 -0700

    Comment out GPIO pins for LED

--- touch_screen/ui/mainWindow.py
+++ touch_screen/ui/mainWindow.py
@@ -7,14 +7,14 @@ Module implementing MainWindow.
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QDate,  QTime,  QDateTime,  Qt
-from gpiozero import LED
+# from gpiozero import LED
 from time import sleep
 
 from .Ui_mainWindow import Ui_MainWindow
 
-led_red = LED(17)
-led_green = LED(27)
-led_blue = LED(22)
+# led_red = LED(17)
+# led_green = LED(27)
+# led_blue = LED(22)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """
@@ -29,7 +29,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        led_red.on()
+        # led_red.on()
     
     @pyqtSlot()
     def on_openBox_clicked(self):
@@ -41,9 +41,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         #date = QDate.currentDate()
         #time = QTime.currentTime()
         self.time.setText("Box opened")
-        led_red.off()
-        sleep(1)
-        led_green.on() 
+        # led_red.off()
+        # sleep(1)
+        # led_green.on() 
         
 
     
