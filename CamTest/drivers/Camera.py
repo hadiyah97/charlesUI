fc90a6bb7cbb79a408eb86c55ba75c96f95e1a6c
pat <phat.ngotan@yahoo.com>
2020-08-02 14:29:12 -0700

    updating camera

--- CamTest/drivers/Camera.py
+++ CamTest/drivers/Camera.py
@@ -223,7 +223,6 @@ class Camera:
                 self.stopRecording()
 
     # Finding the target (room number sign)
-    # Conditions:
     def findTarget(self):
         (W, H) = (None, None)
         (newW, newH) = (320, 320)
@@ -251,9 +250,9 @@ class Camera:
 
             blob = cv2.dnn.blobFromImage(temp_frame, 1.0, (newW, newH), (123.68, 116.78, 103.94), swapRB=True, crop=False)
             net.setInput(blob)
-            (scores, geometry) = net.forward(layerNames)
+            (scores, geo) = net.forward(layerNames)
 
-            (rects, confidences) = self.decode_predictions(scores, geometry, 0.5)
+            (rects, confidences) = self.decoding(scores, geo, 0.5)
             boxes = non_max_suppression(np.array(rects), probs=confidences)
 
 
@@ -308,37 +307,102 @@ class Camera:
             elif key == ord('s'):
                 self.stopRecording()
 
-    def decode_predictions(self, scores, geometry, min_confidence):
+    def decoding(self, scores, geo, minConf):
         (numRows, numCols) = scores.shape[2:4]
         rects = []
         confidences = []
         for y in range(0, numRows):
             scoresData = scores[0, 0, y]
-            xData0 = geometry[0, 0, y]
-            xData1 = geometry[0, 1, y]
-            xData2 = geometry[0, 2, y]
-            xData3 = geometry[0, 3, y]
-            anglesData = geometry[0, 4, y]
+            xData0 = geo[0, 0, y]
+            xData1 = geo[0, 1, y]
+            xData2 = geo[0, 2, y]
+            xData3 = geo[0, 3, y]
+            anglesData = geo[0, 4, y]
             for x in range(0, numCols):
-                if scoresData[x] < min_confidence:
+                if scoresData[x] < minConf:
                     continue
                 (offsetX, offsetY) = (x * 4.0, y * 4.0)
-
                 angle = anglesData[x]
                 cos = np.cos(angle)
                 sin = np.sin(angle)
-
                 h = xData0[x] + xData2[x]
                 w = xData1[x] + xData3[x]
-
                 endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
                 endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
                 startX = int(endX - w)
                 startY = int(endY - h)
-
                 rects.append((startX, startY, endX, endY))
                 confidences.append(scoresData[x])
         return (rects, confidences)
+
+
+    # Finding the target (room number sign)
+    # roomIn - input room number
+    def findTarget2(self, dest):
+        (W, H) = (None, None)
+        (newW, newH) = (320, 320)
+        layerNames = ["feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"]
+        net = cv2.dnn.readNet("drivers/east_detector.pb")
+
+        cnt = 1
+        match = 0
+        while True:
+            self.captureFrame()
+
+            temp_frame = self.frame.copy()
+            disp_frame = self.frame.copy()
+
+            (H, W) = temp_frame.shape[:2]
+            rW = W / float(newW)
+            rH = H / float(newH)
+
+            temp_frame = cv2.resize(temp_frame, (newW, newH))
+
+            blob = cv2.dnn.blobFromImage(temp_frame, 1.0, (newW, newH), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+            net.setInput(blob)
+            (scores, geo) = net.forward(layerNames)
+
+            (rects, confidences) = self.decoding(scores, geo, 0.5)
+            boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+            results = []
+            for (startX, startY, endX, endY) in boxes:
+                startX = int(startX * rW)
+                startY = int(startY * rH)
+                endX = int(endX * rW)
+                endY = int(endY * rH)
+        
+                dX = int((endX - startX) * 0.0)
+                dY = int((endY - startY) * 0.0)
+
+                startX = max(0, startX - dX)
+                startY = max(0, startY - dY)
+                endX = min(W, endX + (dX * 2))
+                endY = min(H, endY + (dY * 2))
+
+                roi = disp_frame[startY:endY, startX:endX]
+                config = ("-l eng --oem 1 --psm 7")
+                text = pytesseract.image_to_string(roi, config=config)
+                results.append(((startX, startY, endX, endY), text))
+
+            results = sorted(results, key=lambda r:r[0][1])
+            for ((startX, startY, endX, endY), text) in results:
+                text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+                if text.isdigit():
+                    if int(text) == dest:
+                        match += 1
+
+            if cnt == 5:
+                if match >= int(cnt/2):
+                    #print(int(cnt/2))
+                    #print(cnt)
+                    #print(match)
+                    print("[INFO]\tDestination Found")
+                    break
+                cnt = 0
+                match = 0              
+            cnt += 1
+            
 
 
 
