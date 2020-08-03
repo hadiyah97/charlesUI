e74edc75363d680bd5610ebcfae9673e5af20164
Sean <nolinyc@gmail.com>
2020-08-03 15:34:42 -0700

    Update new design of user interface

--- touch_screen/ui/Ui_mainWindow.py
+++ touch_screen/ui/Ui_mainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/pi/RobotGUI/robotGUI/ui/mainWindow.ui'
+# Form implementation generated from reading ui file '/home/pi/RobotGUI/charles_touchscreen/ui/mainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -12,14 +12,95 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 480)
+        MainWindow.setStyleSheet("background-color: rgb(52, 52, 52)")
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.openBox = QtWidgets.QPushButton(self.centralWidget)
-        self.openBox.setGeometry(QtCore.QRect(300, 200, 200, 60))
+        self.openBox.setGeometry(QtCore.QRect(450, 270, 230, 150))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setWeight(75)
+        self.openBox.setFont(font)
+        self.openBox.setStyleSheet("#openBox {\n"
+"color: rgb(255, 255, 127);\n"
+"background-color: rgb(242, 242, 242, 20) ;\n"
+"border-radius: 20px\n"
+"}")
         self.openBox.setObjectName("openBox")
         self.time = QtWidgets.QLineEdit(self.centralWidget)
-        self.time.setGeometry(QtCore.QRect(300, 10, 200, 30))
+        self.time.setGeometry(QtCore.QRect(150, 100, 500, 45))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.time.setFont(font)
+        self.time.setStyleSheet("#time\n"
+"{\n"
+"color: rgb(255, 255, 255);\n"
+"}")
+        self.time.setAlignment(QtCore.Qt.AlignCenter)
         self.time.setObjectName("time")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setGeometry(QtCore.QRect(105, 20, 590, 51))
+        font = QtGui.QFont()
+        font.setFamily("Piboto [Goog]")
+        font.setPointSize(28)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255)")
+        self.label.setObjectName("label")
+        self.go = QtWidgets.QPushButton(self.centralWidget)
+        self.go.setGeometry(QtCore.QRect(120, 270, 230, 150))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.go.setFont(font)
+        self.go.setStyleSheet("#go {\n"
+"color: rgb(0, 170, 0); \n"
+"background-color: rgb(242, 242, 242, 20) ;\n"
+"border-radius: 20px\n"
+"}")
+        self.go.setObjectName("go")
+        self.comboBox = QtWidgets.QComboBox(self.centralWidget)
+        self.comboBox.setGeometry(QtCore.QRect(120, 180, 120, 50))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.comboBox.setFont(font)
+        self.comboBox.setStyleSheet("#comboBox\n"
+"{\n"
+"color: white;\n"
+"background:  rgb(255, 255, 255)\n"
+"}")
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.progressBar = QtWidgets.QProgressBar(self.centralWidget)
+        self.progressBar.setGeometry(QtCore.QRect(340, 180, 120, 24))
+        self.progressBar.setStyleSheet("#progressBar\n"
+"{\n"
+"text-align: center; \n"
+"}\n"
+"#progressBar::chunk\n"
+"{\n"
+"background-color: rgb(0, 234, 0);\n"
+"width: 7px; \n"
+"margin: 1px;  \n"
+"}")
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName("progressBar")
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
@@ -27,8 +108,17 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("RobotUI", "RobotUI"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Robot Charles"))
         self.openBox.setText(_translate("MainWindow", "Open Box"))
+        self.label.setText(_translate("MainWindow", "Charles Autonomous Service Robot"))
+        self.go.setText(_translate("MainWindow", "GO"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Room 220"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Room 221"))
+        self.comboBox.setItemText(2, _translate("MainWindow", "Room 222"))
+        self.comboBox.setItemText(3, _translate("MainWindow", "Room 223"))
+        self.comboBox.setItemText(4, _translate("MainWindow", "Room 224"))
+        self.comboBox.setItemText(5, _translate("MainWindow", "Room 225"))
+        self.comboBox.setItemText(6, _translate("MainWindow", "Room 226"))
 
 
 if __name__ == "__main__":
--- touch_screen/ui/mainWindow.py
+++ touch_screen/ui/mainWindow.py
@@ -7,14 +7,14 @@ Module implementing MainWindow.
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QDate,  QTime,  QDateTime,  Qt
-# from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 
 from .Ui_mainWindow import Ui_MainWindow
 
-# led_red = LED(17)
-# led_green = LED(27)
-# led_blue = LED(22)
+led_red = LED(17)
+led_green = LED(27)
+led_blue = LED(22)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """
@@ -29,7 +29,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        # led_red.on()
+        led_red.on()
+        self.progressBar.setValue(0)
+        self.time.setText("Hi! Please select a room and press GO")
     
     @pyqtSlot()
     def on_openBox_clicked(self):
@@ -41,9 +43,52 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         #date = QDate.currentDate()
         #time = QTime.currentTime()
         self.time.setText("Box opened")
-        # led_red.off()
-        # sleep(1)
-        # led_green.on() 
-        
-
+        led_red.off()
+        sleep(1)
+        led_green.on() 
     
+    @pyqtSlot()
+    def on_go_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        #raise NotImplementedError
+        self.progressBar.setValue(0)
+        self.time.setText("Finding the room...")
+        self.time.repaint()
+        self.progressBar.setValue(5)
+        sleep(1.5)
+        self.progressBar.setValue(23)
+        self.time.setText("Planning the route...")
+        self.time.repaint()
+        sleep(1)
+        self.progressBar.setValue(38)
+        sleep(3)
+        self.progressBar.setValue(75)
+        self.time.setText("Charles is ready! Heading to Room 226")
+        sleep(1.7)
+        self.progressBar.setValue(100)
+        #Set GPIO to high to trigger SJ2 
+    
+    @pyqtSlot(str)
+    def on_comboBox_activated(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        # TODO: not implemented yet
+        #raise NotImplementedError
+
+    @pyqtSlot()
+    def on_progressBar_customContextMenuRequested(self, pos):
+        """
+        Slot documentation goes here.
+        
+        @param pos DESCRIPTION
+        @type QPoint
+        """
+        # TODO: not implemented yet
+        #raise NotImplementedError
